e05d3b0759be33f480815a6ee1afdd501efc04af
Muhammet Mirac <113796231+Muhammetmirac@users.noreply.github.com>
2023-06-06 16:13:57 +0300

    v06.06.2023

---
+++ Temperature-Convert/script.js
@@ -0,0 +1,35 @@
+const celsiusEl = document.getElementById("celsius");
+const fahrenheitEl = document.getElementById("fahrenheit");
+const kelvinEl = document.getElementById("kelvin");
+const btnclearEl= document.getElementById("btnclear")
+
+
+function computeTemp(event){
+    const currentValue =+ event.target.value;
+
+    switch (event.target.name) {
+        case "celsius":
+            kelvinEl.value = (currentValue+273.32).toFixed(2);
+            fahrenheitEl.value = (currentValue*1.8 + 32).toFixed(2);
+            
+            break;
+        case "fahrenheit":
+            kelvinEl.value = ((currentValue-32) + 273.32).toFixed(2);
+            celsiusEl.value = ((currentValue-32)/1.8).toFixed(2);
+            break;
+        case "kelvin":
+            fahrenheitEl.value = ((currentValue-273.32)*1.8+32).toFixed(2);
+            celsiusEl.value = ((currentValue-273.32)).toFixed(2);
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function btnclear (){
+    celsiusEl.value=""; 
+    fahrenheitEl.value="";
+    kelvinEl.value="";
+}
+btnclearEl.addEventListener("click" , btnclear);
